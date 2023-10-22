27128bcbe59383702753a222950160031711125a
piefeil <ana.s.silva@ufv.br>
2023-10-22 19:17:19 -0300

    test: tests/loop/test02.py conteúdo adicionado

--- tests/loop/test01.py
+++ tests/loop/test01.py
@@ -3,7 +3,7 @@ import pandas as pd
 tabela = pd.read_excel("curtidas.xlsx")
 
 tabela_enviado = tabela['Para quem gostaria de enviar a curtida?']
-print(tabela_enviado)
+#print(tabela_enviado)
 
 lucas = []
 
@@ -11,9 +11,14 @@ for nome in tabela_enviado:
     if nome == "Lucas Silva":
         lucas.append(nome)
 
-print(lucas)
+# print(lucas)
 
 contagem_lucas = len(lucas)
 
 print(contagem_lucas)
+
+nomes = []
+
+for nome in tabela_enviado:
+    nomes.append(nome)
 
--- tests/loop/test02.py
+++ tests/loop/test02.py
@@ -4,10 +4,37 @@ tabela = pd.read_excel("curtidas.xlsx")
 
 tabela_enviado = tabela['Para quem gostaria de enviar a curtida?']
 
+
+
+
 nomes = []
 
 for nome in tabela_enviado:
-    nomes.append(nome)
+    nomes.append(nome) # Adiciona todos os nomes (repetidos) em uma lista 
 
-print(nomes)
+print (nomes.count("Lucas Silva")) # Conta quantas vezes "Lucas Silva" apareceu
 
+
+
+
+nomes_unicos = set() # Armazena elementos únicos
+
+for nome in tabela_enviado:
+    if nome not in nomes_unicos:
+        nomes_unicos.add(nome) # Adiciona uma fez na lista todos os nomes que tem na tabela
+
+print (nomes_unicos)
+
+
+
+def contagem_nomes(nome):
+    print (nome, nomes.count(nome))
+
+
+
+for nome in nomes_unicos:
+    contagem_nomes(nome)
+
+
+# Adicionar uma forma de somar as 2 colunas 
+# Organizar lista em ordem decrescente
